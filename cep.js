f4c162957f5f942d0c9d77a7bc20146d5b1e5fc2
Miguel V_DF <miguelvittadev@gmail.com>
2024-07-04 21:35:19 -0300

    implemented a async function with proper error handling

--- cep.js
+++ cep.js
@@ -1,9 +1,9 @@
-function getAddressByCep() {
+async function getAddressByCep() {
     const cep = document.getElementById('cep').value;
-    fetch(`https://viacep.com.br/ws/${cep}/json/`).
-    then((response) =>{
-        return response.json();
-    }).then(response =>{
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         console.log(response);
-    });
+    } catch (error) {
+        alert(error.message)
+    }
   }
