42eaa57c318bac960383a2b97b930aceaa4b90b9
Miguel V_DF <miguelvittadev@gmail.com>
2024-07-04 22:08:18 -0300

    implemented the 2 apis properly

--- cep.js
+++
@@ -1,9 +0,0 @@
-async function getAddressByCep() {
-    const cep = document.getElementById('cep').value;
-    try {
-        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        console.log(response);
-    } catch (error) {
-        alert(error.message)
-    }
-  }
